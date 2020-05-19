b0a2e2a3e677976cdde9d0c9817c52fc346ef60c
hello9geg <1147516839@qq.com>
2020-05-19 19:28:42 +0800

    测试

--- src/main/java/com/springboot/smslogin/controller/IndexController.java
+++ src/main/java/com/springboot/smslogin/controller/IndexController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/indexController")
 public class IndexController {
 
+
     @RequestMapping("/index")
     public String index(){
         return "index";
